2fbc06103e11e0ed0f8c1841e613a34f34949e11
amit <amit.prajapati16007@gmail.com>
2021-04-24 12:55:22 +0530

    first commite

---
+++ InvoiceBL/Context/InvoiceContext.cs
@@ -0,0 +1,34 @@
+﻿using InvoiceBL.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceBL.Context
+{
+    public class InvoiceContext : DbContext 
+    {
+        public InvoiceContext() : base("name = MydbConn")
+        {
+            Database.SetInitializer(new InvoiceDBInitializer());
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+    public class InvoiceDBInitializer : DropCreateDatabaseAlways<InvoiceContext>
+    {
+        protected override void Seed(InvoiceContext context)
+        {
+            IList<Customer> defaultStandards = new List<Customer>();
+
+            defaultStandards.Add(new Customer() { ID = 1, Name = "First Standard" });
+            defaultStandards.Add(new Customer() { ID = 2, Name = "Second Standard" });
+            defaultStandards.Add(new Customer() { ID = 3, Name = "Third Standard" });
+
+            context.Customers.AddRange(defaultStandards);
+
+            base.Seed(context);
+        }
+    }
+}
---
+++ InvoiceBL/Model/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceBL.Model
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR")]
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
